9309695c4288ef57eeacfd96bfb95193ce14cf54
Naveenmayur <naveenmayurkr@gmail.com>
2026-04-26 11:12:38 +0100

    feat(auth-service): add token validation endpoint
    - Implemented GET /api/auth/validate endpoint in AuthController to verify JWT integrity.
    - Added validateToken logic to AuthService and JwtUtil using JJWT parser.
    - Optimized imports in AuthController for better readability.
    - Configured error handling for invalid or malformed tokens during validation.

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -2,16 +2,12 @@ package com.pm.authservice.controller;
 
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
-import com.pm.authservice.model.User;
 import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -31,5 +27,14 @@ public class AuthController {
         return token.map(s -> ResponseEntity.ok(new LoginResponseDTO(s)))
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String token) {
+        if(token==null || !token.startsWith("Bearer "))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        boolean isValid = authService.validateToken(token.substring(7)); // Remove "Bearer " prefix
+        return isValid ? ResponseEntity.ok().build() : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }

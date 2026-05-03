866d1655fd5a2a4cc5d5e7e883bae2055169786c
Naveenmayur <naveenmayurkr@gmail.com>
2026-05-03 16:58:13 +0100

    Add infrastructure module with AWS CDK and LocalStack configuration
    - Create new 'infrastructure' module for AWS resource management using CDK.
    - Add LocalStack.java for local AWS environment setup.
    - Update IDE configuration (.idea) to include the new module.
    - Add .gitignore to infrastructure and integration-tests to ignore target directories.

---
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -0,0 +1,17 @@
+package com.pm.stack;
+
+import software.amazon.awscdk.*;
+import software.constructs.Construct;
+
+public class LocalStack extends Stack {
+
+    public LocalStack(final App scope, final String id, final StackProps props) {
+        super(scope, id, props);
+    }
+
+    public static void main(String[] args) {
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
+
+        StackProps props = StackProps.builder().synthesizer(new BootstraplessSynthesizer()).build();
+    }
+}

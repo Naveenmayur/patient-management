2303b215cfd6af1f48bc436e025851175733c28f
Naveenmayur <naveenmayurkr@gmail.com>
2026-05-04 18:24:51 +0100

    Expand infrastructure with VPC, RDS, MSK, and ECS; Downgrade Java to 17
    - Expand LocalStack.java with VPC, RDS instances, Health Checks, and MSK cluster.
    - Implement createEcsCluster and correct ECS imports in LocalStack.java.
    - Downgrade Java version to 17 in infrastructure and integration-tests poms for environment compatibility.
    - Add cdk.out/ to infrastructure/.gitignore.

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,17 +1,98 @@
 package com.pm.stack;
 
+import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.*;
-import software.constructs.Construct;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.msk.CfnCluster;
+import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 public class LocalStack extends Stack {
 
+    private final Vpc vpc;
+    private final Cluster ecsCluster;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+        this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb = createDatabaseInstance("AuthServiceDb", "auth-service-db");
+        DatabaseInstance patientServiceDb = createDatabaseInstance("PatientServiceDb", "patient-service-db");  
+
+        CfnHealthCheck authDbHealthCheck = createHealthCheck(authServiceDb, "AuthServiceDbHealthCheck");       
+        CfnHealthCheck patientDbHealthCheck = createHealthCheck(patientServiceDb, "PatientServiceDbHealthCheck");
+
+        CfnCluster mskCluster = createMskCluster();
+
+        this.ecsCluster = createEcsCluster();
+    }
+
+    private Cluster createEcsCluster() {
+        return Cluster.Builder.create(this, "PatientManagementCluster")
+                .vpc(this.vpc)
+                .clusterName("PatientManagementCluster")
+                .build();
+    }
+    private Vpc createVpc() {
+        return Vpc.Builder
+                .create(this, "PatientManagementVpc")
+                .vpcName("PatientManagementVpc")
+                .maxAzs(2).build();
+    }
+
+    private DatabaseInstance createDatabaseInstance(String id, String dbName) {
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .instanceIdentifier(id)
+                .databaseName(dbName)
+                .engine(DatabaseInstanceEngine.postgres(PostgresInstanceEngineProps.builder()
+                        .version(PostgresEngineVersion.VER_18).build()))
+                .vpc(this.vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
+    private CfnHealthCheck createHealthCheck(DatabaseInstance dbInstance, String id) {
+        return CfnHealthCheck.Builder
+                .create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP")
+                        .port(Token.asNumber(dbInstance.getDbInstanceEndpointPort()))
+                        .ipAddress(dbInstance.getDbInstanceEndpointAddress())
+                        .requestInterval(30)
+                        .failureThreshold(3)
+//                        .resourcePath("/")
+                        .build())
+                .build();
+    }
+
+    private CfnCluster createMskCluster() {
+        return CfnCluster.Builder
+                .create(this, "MskCluster")
+                .clusterName("kafka-cluster")
+                .kafkaVersion("2.8.1")
+                .numberOfBrokerNodes(1)
+                .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.xlarge")
+                        .clientSubnets(this.vpc.getPrivateSubnets().stream()
+                                .map(ISubnet::getSubnetId)
+                                .toList())
+                        .brokerAzDistribution("DEFAULT")
+                        .build())
+                .build();
     }
 
     public static void main(String[] args) {
         App app = new App(AppProps.builder().outdir("./cdk.out").build());
 
         StackProps props = StackProps.builder().synthesizer(new BootstraplessSynthesizer()).build();
+
+        new LocalStack(app, "LocalStack", props);
+        app.synth();
+        System.out.println("CDK app synthesized successfully.");
     }
 }
